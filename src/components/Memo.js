372923f936b133dbffb13be80b89489d36d11541
hari <harikarumuri1086@gmail.com>
2022-05-31 18:00:01 +0530

    added memo

---
+++ src/components/Memo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Memo = () => {
+  return (
+    <div>
+      <h3>Memo for the day!!</h3>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+    </div>
+  )
+}
+
+export default Memo
---
+++ src/components/Top-Priority.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopPriority = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TopPriority
